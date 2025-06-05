cba44123460d43bd0c08562fca3d98b1337aff42
KleeSky <kleesky@foxmail.com>
2025-06-05 17:28:03 +0800

    添加 ”变量的使用“

---
+++ 03 C++ 变量的使用/C++ 变量的使用.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//语法：数据类型 变量名 = 值;
+	int a = 10; // 定义一个整型变量a并初始化为10
+	cout << "a的值为：" << a << endl; // 输出变量a的值
+	system("pause");
+	return 0;
+}
