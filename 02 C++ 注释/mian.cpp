e2140cd6d14c39da78ee5c0ad8da9bf37661bfd3
KleeSky <kleesky@foxmail.com>
2025-06-05 14:19:27 +0800

    添加 “注释”

---
+++ 02 C++ 注释/mian.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	main函数是C++程序的一个入口，
+	每个C++程序都必须有一个main函数，
+	而且有且仅有一个main函数。
+*/
+/*
+	这是一个多行注释
+*/
+
+int main()
+{
+	// 这是一个单行注释
+	// 输出Hello World!
+	cout << "Hello World!" << endl;
+	system("pause");
+	return 0;
+}
